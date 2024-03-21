e0d236b1ad740bf7cea8c7bcb93b452826caeccb
habtew <habtamuwolde92@gmail.com>
2024-03-21 21:39:49 +0530

    setInterval

--- timeout.js
+++ timeout.js
@@ -2,3 +2,10 @@ const waitTime = 3000
 console.log(`setting a ${waitTime / 1000} second delay`)
 const timerFinished = ()=>console.log("done")
 setTimeout(timerFinished, waitTime)
+const waitInterval = 500
+let current = 0
+const inTime = ()=>{
+    current += waitInterval
+    console.log(`waiting ${current / 1000} seconds`)
+}
+setInterval(inTime, waitInterval)
