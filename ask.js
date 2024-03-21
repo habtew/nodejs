b5fbaafd50ce4d8a4d29767753758b0ef28cd984
habtew <habtamuwolde92@gmail.com>
2024-03-21 22:16:29 +0530

    question answer with readline

--- ask.js
+++ ask.js
@@ -5,6 +5,31 @@ const rl = readline.createInterface({
     output: process.stdout
 })
 
-rl.question("how are you doing?", answer=>{
-    console.log(`your anser: ${answer}`)
+const questions = [
+    "what is your name? ",
+    "how old are you? ",
+    "what are doing tonight? "
+]
+
+function collectAnswers(questions, done) {
+    const answers = []
+    const questionAnswered = answer => {
+        answers.push(answer.trim())
+        if (answers.length < questions.length){
+            rl.question(
+                questions[answers.length],
+                questionAnswered
+            )
+        }
+        else{
+            return done(answers)
+        }
+    }
+    rl.question(questions[0], questionAnswered)
+}
+
+collectAnswers(questions, (answers)=>{
+    console.log("Thank you for your answers")
+    console.log(answers)
+    process.exit()
 })
